6c8773b6e8b977f07bbc632611096a47236eb873
kapich1192 <kapich1192@yandex.ru>
2022-08-04 16:10:41 +0300

    First commit

---
+++ src/main/java/com/learningInAction/LearningInActionApplication.java
@@ -0,0 +1,13 @@
+package com.learningInAction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningInActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learningInAction/LearningInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningInAction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
