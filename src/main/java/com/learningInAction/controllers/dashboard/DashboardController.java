834d6de0820a923e3100dd6b0aa58212f7403e8b
kapich1192 <kapich1192@yandex.ru>
2022-08-07 15:21:43 +0300

    develop Step #2

--- src/main/java/com/learningInAction/controllers/dashboard/DashboardController.java
+++ src/main/java/com/learningInAction/controllers/dashboard/DashboardController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
+    /*================================================= FIELDS =======================================================*/
+    private String title = "Dashboard";
+    /*================================================= GET ==========================================================*/
     @GetMapping("/dashboard")
     public String getDashboard(Model model) {
+        model.addAttribute("title", title);
         return "pages/dashboard/dashboard";
     }
+    /*================================================= POST =========================================================*/
 }
