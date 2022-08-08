b1e91a03d0926aac07adbb2729b5c084cba5eacf
kapich1192 <kapich1192@yandex.ru>
2022-08-08 12:18:26 +0300

    develop Step #3

--- src/main/java/com/learningInAction/controllers/intersting/news/NewsController.java
+++ src/main/java/com/learningInAction/controllers/intersting/news/NewsController.java
@@ -1,6 +1,8 @@
-package com.learningInAction.controllers.dashboard;
+package com.learningInAction.controllers.intersting.news;
 
+import com.learningInAction.model.news.News;
 import com.learningInAction.model.user.User;
+import com.learningInAction.repo.news.NewsRepo;
 import com.learningInAction.repo.user.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -10,21 +12,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class DashboardController {
+public class NewsController {
     /*================================================= FIELDS =======================================================*/
-    private String title = "Dashboard";
+    private String title = "News";
     @Autowired
     UserRepo userRepo;
+    @Autowired
+    NewsRepo newsRepo;
     /*================================================= GET ==========================================================*/
-    @GetMapping("/dashboard")
-    public String getDashboard(Model model) {
-        /*+++++ Meta +++++*/
+    /*get all news*/
+    @GetMapping("/news")
+    public String getNews(Model model) {
         model.addAttribute("title", title);
-        /*+++++ User +++++*/
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         User user = userRepo.findByUsername(auth.getName());
-        model.addAttribute("usr",user);
-        return "pages/dashboard/dashboard";
+        model.addAttribute("usr", user);
+        Iterable<News> news = newsRepo.findAll();
+        model.addAttribute("news", news);
+        return "pages/news/news";
     }
     /*================================================= POST =========================================================*/
 }
